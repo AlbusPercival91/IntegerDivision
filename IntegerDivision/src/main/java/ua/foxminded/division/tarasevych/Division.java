7bd33b6690fce7361e743c0598fa159482efe586
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2022-10-05 21:36:34 +0300

    commit to save temp results

--- IntegerDivision/src/main/java/ua/foxminded/division/tarasevych/Division.java
+++ IntegerDivision/src/main/java/ua/foxminded/division/tarasevych/Division.java
@@ -27,6 +27,7 @@ public class Division {
     }
 
     public static int divisionEngine(int dividend, int divider) {
+        List<Integer> buildList = new ArrayList<>();
         StringBuilder builder = new StringBuilder();
         int dividerLength = String.valueOf(divider).length();
         int result = 0;
@@ -49,10 +50,10 @@ public class Division {
                     builder.append(0);
                 }
             }
-            if (substraction != 0) {
-                System.out.println(substraction + "\n" + variable);
-            }
 
+            if (substraction != 0) {
+                Collections.addAll(buildList, substraction, variable);
+            }
         }
 
         if (dividerLength == getDigits(dividend).size()) {
@@ -63,12 +64,28 @@ public class Division {
             result = Integer.parseInt(builder.toString());
 
             if (substraction > variable) {
-                System.out.println(substraction + "\n" + variable);
+                Collections.addAll(buildList, substraction, variable);
             }
         }
-        System.out.println("= " + result);
-
+        divisionDraw(buildList, dividend, divider, result);
         return result;
+    }
+
+    public static void divisionDraw(List<Integer> list, int dividend, int divider, int result) {
+        System.out.printf("_%d|%d", dividend, divider);
+        System.out.println("");
+        System.out.printf(" %-" + String.valueOf(dividend).length() + "d|%s", list.get(0), "-----");
+        System.out.println("");
+        System.out.printf(" %-" + String.valueOf(dividend).length() + "s|%d", "-", result);
+        System.out.println("");
+
+        for (int i = 1; i < list.size(); i++) {
+            System.out.printf("% d\n", list.get(i));
+
+            if (i % 2 == 0) {
+                System.out.printf(" %s\n", "--");
+            }
+        }
     }
 
 }
