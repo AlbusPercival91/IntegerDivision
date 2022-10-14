f845b01dcc76b00d16ea5805bcc082e3d66d6fbb
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2022-10-15 01:06:26 +0300

    change to new plan with method convertToView

--- IntegerDivision/src/main/java/ua/foxminded/division/tarasevych/Division.java
+++ IntegerDivision/src/main/java/ua/foxminded/division/tarasevych/Division.java
@@ -5,7 +5,6 @@ import java.util.Collections;
 import java.util.List;
 
 public class Division {
-    static List<Integer> varLengthDifList = new ArrayList<>();
 
     private Division() {
 
@@ -48,7 +47,6 @@ public class Division {
             result = Math.abs(variable / divider);
             builder.append(result);
             substraction = Math.abs(result * divider);
-            int variableLength = String.valueOf(variable - substraction).length();
             variable = Integer.parseInt(Math.abs(variable - substraction) + "".concat(getAllDigits(dividend)
                     .get(dividerLength++).toString().replace("[", "").replace("]", "").replace(", ", "")));
 
@@ -63,7 +61,6 @@ public class Division {
 
             if (substraction != 0) {
                 Collections.addAll(buildList, substraction, variable);
-                Collections.addAll(varLengthDifList, String.valueOf(substraction).length() - variableLength, 0);
             }
         }
 
@@ -76,59 +73,64 @@ public class Division {
 
             if (substraction > variable) {
                 Collections.addAll(buildList, substraction, variable);
-                Collections.addAll(varLengthDifList,
-                        String.valueOf(substraction).length() - String.valueOf(variable).length(), 0);
             }
         }
         return converToView(buildList, dividend, divider, result);
     }
 
     public static String converToView(List<Integer> list, int dividend, int divider, int result) {
+        List<Integer> digitLengthList = new ArrayList<>();
         StringBuilder stringBuilder = new StringBuilder(String.format("_%d|%d" + "\n", dividend, divider));
         int resultLength = String.valueOf(result).length();
-        int i = 0;
-        int j = 0;
-        int sum = 0;
+        digitLengthList.add(String.valueOf(concatDigits(dividend, list.get(0))).length());
+        int dif = 0;
+        for (int i = 0; i < list.size(); i++) {
 
-        while (i < list.size() && j < varLengthDifList.size()) {
-            int dif = String.valueOf(concatDigits(dividend, list.get(i))).length()
-                    - String.valueOf(list.get(i)).length();
-            int space = String.valueOf(dividend).length() - String.valueOf(list.get(i)).length();
-
-            if (dif > 0) {
-                space = String.valueOf(dividend).length() - String.valueOf(list.get(i)).length() - 1;
+            if (i % 2 == 0) {
+                Collections.addAll(digitLengthList, String.valueOf(list.get(i)).length(),
+                        String.valueOf(list.get(i + 1)).length());
             }
 
+//            int dif = String.valueOf(concatDigits(dividend, list.get(i))).length()
+//                    - String.valueOf(list.get(i)).length();
+
+            /*
+             * change negative resulte (-1) to 0
+             */
+
+            dif = digitLengthList.get(i) - digitLengthList.get(i + 1);
+            System.out.println(dif);
+
             if (i < 1) {
+                int space = String.valueOf(dividend).length() - String.valueOf(list.get(i)).length();
+
+                if (dif > 0) {
+                    space = String.valueOf(dividend).length() - String.valueOf(list.get(i)).length() - 1;
+                }
+
                 stringBuilder.append(String.format(" %s" + "%d" + "%s" + "%s" + "%s" + "\n",
-                        String.join("", Collections.nCopies(dif, " ")), list.get(i),
+                        String.join("", Collections.nCopies(0, " ")), list.get(i),
                         String.join("", Collections.nCopies(space, " ")), "|",
                         String.join("", Collections.nCopies(resultLength, "-"))));
 
                 stringBuilder.append(String.format(String.format(" %s" + "%s" + "%s" + "%s" + "%d" + "\n",
-                        String.join("", Collections.nCopies(dif, " ")),
+                        String.join("", Collections.nCopies(0, " ")),
                         String.join("", Collections.nCopies(String.valueOf(list.get(i)).length(), "-")),
                         String.join("", Collections.nCopies(space, " ")), "|", result)));
             }
 
             if (i >= 1) {
-                sum += varLengthDifList.get(j++);
+                stringBuilder.append(
+                        String.format(" %s" + "%d" + "\n", String.join("", Collections.nCopies(0, " ")), list.get(i)));
 
-                if (divider == list.get(j)) {
-                    sum++;
-                }
-
-                stringBuilder.append(String.format(" %s" + "%d" + "\n", String.join("", Collections.nCopies(sum, " ")),
-                        list.get(j)));
-
-                if (j % 2 == 0) {
+                if (i % 2 == 0) {
                     stringBuilder
-                            .append(String.format(" %s" + "%s" + "\n", String.join("", Collections.nCopies(sum, " ")),
-                                    String.join("", Collections.nCopies(String.valueOf(list.get(j)).length(), "-"))));
+                            .append(String.format(" %s" + "%s" + "\n", String.join("", Collections.nCopies(0, " ")),
+                                    String.join("", Collections.nCopies(String.valueOf(list.get(i)).length(), "-"))));
                 }
             }
-            i++;
         }
+        System.out.println(digitLengthList);
         return stringBuilder.toString();
     }
 }
